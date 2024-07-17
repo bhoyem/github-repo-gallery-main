0028740b85d19eae27647c3ae1a4135dd778763e
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-07-17 17:38:42 -0500

    Added code to fetch repos

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
 //This div is where my profile information will appear
 const overviewDiv = document.querySelector(".overview");
 const username = "bhoyem";
+const repoList = document.querySelector(".repo-list");
 
-const fetchUsers = async function () {
+const userInfo = async function () {
     const userData = await fetch(`https://api.github.com/users/${username}`);
     const fetchedData = await userData.json();
-    console.log(fetchedData);
+    //console.log(fetchedData);
     displayUserInfo(fetchedData);
     return fetchedData;
-}
+};
 
 const displayUserInfo = function (fetchedData) {
     let div = document.createElement("div")
     div.classList.add("user-info");
     div.innerHTML = `
-    <figure>
-    <img alt="user avatar" src=${fetchedData.avatar_url} />
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${fetchedData.name}</p>
-    <p><strong>Bio:</strong> ${fetchedData.bio}</p>
-    <p><strong>Location:</strong> ${fetchedData.location}</p>
-    <p><strong>Number of public repos:</strong> ${fetchedData.public_repos}</p>
-  </div>`;
-  overviewDiv.append(div);
-}
+        <figure>
+            <img alt="user avatar" src=${fetchedData.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${fetchedData.name}</p>
+            <p><strong>Bio:</strong> ${fetchedData.bio}</p>
+            <p><strong>Location:</strong> ${fetchedData.location}</p>
+            <p><strong>Number of public repos:</strong> ${fetchedData.public_repos}</p>
+        </div>
+    `;
+    overviewDiv.append(div);
+    fetchRepos(fetchedData);
+};
 
-fetchUsers();
+userInfo();
+
+const fetchRepos = async function (fetchedData) {
+    const userRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const fetchedRepos = await userRepos.json();
+    console.log(fetchedRepos);
+    repoInfo(fetchedRepos);
+};
+
+//fetchRepos();
+
+const repoInfo = function (repos) {
+    for (let repo of repos) {
+        const listItem = document.createElement("li");
+        listItem.classList.add("repo");
+        listItem.innerHTML = `<h3>"${repo.name}"</h3>`;
+        repoList.append(listItem);
+    }
+};
