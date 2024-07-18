e27669c8b538c5d491e39cda1eb5b998a2f291d7
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-07-18 11:13:45 -0500

    Fixed a bug with the button to view a repo on github

--- js/script.js
+++ js/script.js
@@ -83,7 +83,7 @@ const repoInfoDisplay = function (repoInfo, languages) {
         <p>Description: ${repoInfo.description}</p>
         <p>Default Branch: ${repoInfo.default_branch}</p>
         <p>Languages: ${languages.join(", ")}</p>
-        <a class="visit" href="${repoInfo.git_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
     `;
     repoData.append(div);
     repoData.classList.remove("hide");
